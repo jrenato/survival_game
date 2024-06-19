1a244ab6ab85abf51d2cff0f9b2dfd2c174aeb7d
karvalho <renato@karvalho.com.br>
2024-06-18 21:26:23 -0300

    Added torch and campfire sounds

--- Items/Interactables/interactable_cooker.gd
+++ Items/Interactables/interactable_cooker.gd
@@ -16,12 +16,14 @@ var cooking_recipe: CookingRecipeResource
 @onready var cooking_timer = %CookingTimer
 @onready var fire_particles = %GPUParticles3D
 @onready var fire_light = %OmniLight3D
+@onready var audio_stream_player: AudioStreamPlayer3D = %AudioStreamPlayer3D
 
 
 func _ready() -> void:
 	if fire_always_on:
 		fire_particles.emitting = true
 		fire_light.show()
+		audio_stream_player.play()
 
 
 func start_interaction() -> void:
@@ -66,6 +68,7 @@ func cooking_started() -> void:
 	if not fire_always_on:
 		fire_particles.emitting = true
 		fire_light.show()
+		audio_stream_player.play()
 
 
 func cooking_finished() -> void:
@@ -76,5 +79,6 @@ func cooking_finished() -> void:
 	if not fire_always_on:
 		fire_particles.emitting = false
 		fire_light.hide()
+		audio_stream_player.stop()
 
 	food_visuals_holder.add_child(cooking_recipe.cooked_item_visuals.instantiate())
